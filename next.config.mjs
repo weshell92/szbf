565b3499404c7707c1df83368f7767e51aab5eda
weshell92 <29472639+weshell92@users.noreply.github.com>
2026-07-01 23:28:54 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  trailingSlash: true,
+  basePath: '/szbf',
+  assetPrefix: '/szbf/',
+  images: {
+    unoptimized: true
+  },
   experimental: {
-    mdxRs: true
+    optimizePackageImports: ['lucide-react']
   }
 };
+
 export default nextConfig;
